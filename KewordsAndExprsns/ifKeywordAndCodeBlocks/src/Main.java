ac31f16864d8001c64f0ab8154ce6dfaa1a0de5b
mikiemapo <findmikemm@gmail.com>
2023-09-09 22:04:24 -0400

    if statement update

--- KewordsAndExprsns/ifKeywordAndCodeBlocks/src/Main.java
+++ KewordsAndExprsns/ifKeywordAndCodeBlocks/src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 5000;
+        int score = 4000;
         int levelCompleted = 5;
-        int bonus = 1000;
-
-        if (score == 5000) {
-            System.out.println(" Yore score was 5000");
+        int bonus = 100;
+            // condition
+        if (score < 5000 && score > 1000) {
+            // code block to execute iof conditions true
+            System.out.println(" Your score was less than 5000 but greater than 1000");
+            // condition
+        } else if (score < 1000){
+            // code block to execute iof conditions true
+            System.out.println(" Your score was less than 1000 ");
+            // condition
+        } else {
+            // code block to execute iof conditions true
+            System.out.println(" Got here ");
         }
     }
 }
