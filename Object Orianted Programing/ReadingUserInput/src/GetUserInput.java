3312aca9d1f6e43f124cad401f42f476613f76f8
mikiemapo <findmikemm@gmail.com>
2023-09-24 00:15:01 -0400

    scanner set and tests phase

--- Object Orianted Programing/ReadingUserInput/src/GetUserInput.java
+++ Object Orianted Programing/ReadingUserInput/src/GetUserInput.java
@@ -1,20 +1,42 @@
+import java.util.Scanner;
+
 public class GetUserInput {
 
     public static void main(String[] args) {
-        gettingInputFromUser(2023);
+        int currentYear = 2023;
+
+        try {
+            System.out.println(gettingInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
-    public static void gettingInputFromUser(int currentYear) {
+    public static String gettingInputFromConsole(int currentYear) {
         String name = System.console().readLine("Hi, what's your Name: ");
         System.out.println("Hi " + name + ", Thanks for the input");
 
         String dateOfBirth = System.console().readLine("Date of birth: ");
         int age = currentYear - Integer.parseInt(dateOfBirth);
         System.out.println("You are " + age + " years old.");
+        return "So you are " + age;
     }
 
     public static String getInputFromScanner(int currentYear) {
-        // You can implement this method as needed.
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+        //String name = System.console().readLine("Hi, what's your Name: ");
+        System.out.println("Hi whats your name");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Thanks for the input");
+
+        //String dateOfBirth = System.console().readLine("Date of birth: ");
+        System.out.println("What year were you born");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        System.out.println("You are " + age + " years old.");
+
+        return "So you are " + age;
     }
 }
